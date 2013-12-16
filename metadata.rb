9d2f4492eae19e6b01122e3b1c219139e8d2d3fa
bninja <andrew@poundpay.com>
2013-12-15 20:17:17 -0800

    cln

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,7 @@ description      'Installs/Configures balanced-builder'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends          'apt_transport_s3', '~> 0.1.2'
-depends          'fpm', '~> 0.0.1'
+depends          'balanced-apt'
+depends          'balanced-postgres'
 depends          'balanced-python'
+depends          'balanced-app'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,21 +1,15 @@
-#
-# Cookbook Name:: balanced-builder
-# Recipe:: default
-#
-# Copyright (C) 2013 balanced
-# 
-# All rights reserved - Do Not Redistribute
-#
-%w(python-virtualenv python-dev build-essential libpq-dev cython).each do |package_name|
+%w(python-virtualenv python-dev build-essential libpq-dev cython ruby1.9.1 ruby1.9.1-dev).each do |package_name|
   package package_name do
     action :install
   end
 end
 
-cookbook_file "/etc/sudoers.d/deploy" do
-  source "etc/sudoers.d/deploy"
-  owner "root"
-  group "root"
-  mode "0440"
-  action :create_if_missing
+%w(fpm deb-s3).each do |name|
+  gem_package name do
+    action :install
+  end
 end
+
+include_recipe 'balanced-postgres::client'
+
+include_recipe 'balanced-app'
